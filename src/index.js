dfbfb2f0dd1c3304038c030608a6e0be2ab58912
rbeas098 <beasraquel154@gmail.com>
2023-10-03 19:46:47 -0700

    post

--- src/index.js
+++ src/index.js
@@ -2,10 +2,16 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => {
-  res.send('Heeeeellllllloooooooooo wwwwooooooorrrrlllldddd')
-})
+app.get('/', (req, res) => { 
+  console.log("Running root GET request")
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+  var json_message = {
+    message: 'Hellooooooooooooooo Wooooooooooorld!'
+  }
+
+  console.log("Sending response")
+  res.send(json_message)
 })
+app.post('/postRequest', (req,res) => {
+ res.send("hello this is my first post")
+});
